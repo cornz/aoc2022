c32737f3e0fb5955c6a1d720bfbff4e16e3c0d52
cornz <cputzler@googlemail.com>
2022-12-01 09:19:42 +0100

    solve d1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,53 @@
 fun main() {
+
+    fun getBiggestKcal(input: List<String>): ArrayList<Int> {
+        val summedUpValues = arrayListOf<Int>();
+        var summedUpValue = 0;
+        for (it in input) {
+            when (it) {
+                "" -> {
+                    summedUpValues.add(summedUpValue)
+                    summedUpValue = 0;
+                }
+                input[input.lastIndex] -> {
+                    summedUpValue += it.toInt()
+                    summedUpValues.add(summedUpValue)
+                }
+                else -> {
+                    summedUpValue += it.toInt()
+                }
+            }
+        }
+
+        return summedUpValues
+    }
+
+    fun getBiggestKcalMax(input: ArrayList<Int>): Int {
+        return input.max()
+    }
+
+    fun getBiggestKcalSumUpTopThree(input: ArrayList<Int>): Int {
+        val sorted =  input.sortedDescending()
+        return sorted[0]+sorted[1]+sorted[2]
+    }
+
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return getBiggestKcalMax(getBiggestKcal(input))
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return getBiggestKcalSumUpTopThree(getBiggestKcal(input))
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("input/Day01_test")
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
+
+    val input = readInput("input/Day01")
     println(part1(input))
     println(part2(input))
 }
---
+++ src/Day02.kt
@@ -0,0 +1,20 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+
+    
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("input/Day01_test")
+    check(part1(testInput) == 1)
+
+    val input = readInput("input/Day01")
+    println(part1(input))
+    println(part2(input))
+}
